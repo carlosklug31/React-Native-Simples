bf4f0e7f4848e7182a328c3e3536f5ee9f8d0773
carlosklug31 <carlosklug31@gmail.com>
2020-05-05 14:40:48 -0300

    5-mai-20

--- App.js
+++ App.js
@@ -1,19 +1,49 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import Constants from 'expo-constants'
+
+import Form from './components/Form'
+import TodoList from'./components/TodoList'
+
+import {TodoService} from './services/TodoService'
+
+export default class App extends React.Component {
+
+  state = {
+    list: []
+  }
+
+  async componentDidMount(){
+    const list = await TodoList.list();
+    this.setState({list});
+  }
+
+  add = async (text) => {
+    const newItem = await TodoService.create({text});
+    const list = [...this.state.list, newItem];
+    this.setState({list});
+  }
+
+  remove = async (item) => {
+    await TodoService.remove(item.id);
+    const list = this.state.list.filter(itemList => itemList.id !== item.id);
+    this.setState({list});
+  }
+
+  render() {
+    const {state} = this;
+    return (
+      <View style={styles.container}>
+        <Form onAdd={this.add} />
+        <TodoList list={state.list} onRemove={this.remove} />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight
   },
 });
